e2f0844210d5cb6323b2fdfedd3f64e41a76eb4a
Cactusinhand <colin1@foxmail.com>
2025-09-19 16:47:40 +0800

    update

--- filter-repo-rs/src/main.rs
+++ filter-repo-rs/src/main.rs
@@ -1,16 +1,3 @@
-mod message;
-mod pathutil;
-mod gitutil;
-mod backup;
-mod opts;
-mod pipes;
-mod tag;
-mod commit;
-mod filechange;
-mod finalize;
-mod migrate;
-mod stream;
-
 use filter_repo_rs as fr;
 use std::io;
 
